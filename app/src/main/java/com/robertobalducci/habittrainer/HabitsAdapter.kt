fb40e20f9853e52f058f6a8cd4a59023c9e4c2e9
Roberto Balducci <errebi@gmail.com>
2019-02-20 23:46:08 +0100

    section 7 lecture 79

--- app/src/main/java/com/robertobalducci/habittrainer/HabitsAdapter.kt
+++ app/src/main/java/com/robertobalducci/habittrainer/HabitsAdapter.kt
@@ -1,20 +1,37 @@
 package com.robertobalducci.habittrainer
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.single_card.view.*
 
 class HabitsAdapter(val habits: List<Habit>) : RecyclerView.Adapter<HabitsAdapter.HabitViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): HabitViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
+    class HabitViewHolder(val card: View) : RecyclerView.ViewHolder(card)
+
+    override fun onBindViewHolder(holder: HabitViewHolder, index: Int) {
+
+        if (holder != null) {
+            holder.card.tv_title.text = habits[index].title
+            holder.card.tv_description.text = habits[index].description
+            holder.card.iv_icon.setImageResource(habits[index].image)
+        }
     }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent, false)
+        return HabitViewHolder(view)
+    }
+
+
 
     override fun getItemCount(): Int = habits.size
 
-    override fun onBindViewHolder(p0: HabitViewHolder, p1: Int) {
-        TODO("not implemented")
-    }
 
-    class HabitViewHolder(val iv: View) : RecyclerView.ViewHolder(iv)
+
+
 
 }
